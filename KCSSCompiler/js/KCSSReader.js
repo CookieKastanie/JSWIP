64dbca67b60a6e88680c0808eba2cb0f8a02e596
CookieKastanie <jerem0205@yahoo.fr>
2018-08-04 16:55:29 +0200

    ...

--- KCSSCompiler/js/KCSSReader.js
+++ KCSSCompiler/js/KCSSReader.js
@@ -72,6 +72,14 @@ class Words {
 
     return false;
   }
+
+  static mediaCharExist(m){
+    for (let t of Words.inMedia) {
+      if(m == t) return true;
+    }
+
+    return false;
+  }
 }
 
 Words.tags = ["a","abbr","acronym","address","applet","area","article","aside","audio","b","base","basefont","bdo","big","blockquote","body","br","button","canvas","caption","center","cite","code","col","colgroup",
@@ -91,6 +99,8 @@ Words.inParams = ["+", "-", " "];
 Words.separators = [","," ",">","+","~"];
 
 Words.inAttribut = ["=","~","|","^","$","*","_",".","\"","'"," "];
+
+Words.inMedia = [":","-",",","(",")"," "];
 
 Words.properties = ["align-content","align-items","align-self","all","animation","animation-delay","animation-direction","animation-duration","animation-fill-mode","animation-iteration-count","animation-name","animation-play-state","animation-timing-function","backface-visibility","background","background-attachment","background-blend-mode","background-clip","background-color","background-image","background-origin","background-position","background-repeat","background-size","border","border-bottom","border-bottom-color","border-bottom-left-radius","border-bottom-right-radius","border-bottom-style","border-bottom-width","border-collapse","border-color","border-image","border-image-outset","border-image-repeat","border-image-slice","border-image-source","border-image-width","border-left","border-left-color","border-left-style","border-left-width","border-radius","border-right","border-right-color","border-right-style","border-right-width","border-spacing","border-style","border-top","border-top-color","border-top-left-radius","border-top-right-radius","border-top-style","border-top-width","border-width","bottom","box-decoration-break","box-shadow","box-sizing","break-after","break-before","break-inside","caption-side","caret-color","@charset","clear","clip","color","column-count","column-fill","column-gap","column-rule","column-rule-color","column-rule-style","column-rule-width","column-span","column-width","columns","content","counter-increment","counter-reset","cursor","direction","display","empty-cells","filter","flex","flex-basis","flex-direction","flex-flow","flex-grow","flex-shrink","flex-wrap","float","font","@font-face","font-family","font-feature-settings","@font-feature-values","font-kerning","font-language-override","font-size","font-size-adjust","font-stretch","font-style","font-synthesis","font-variant","font-variant-alternates","font-variant-caps","font-variant-east-asian","font-variant-ligatures","font-variant-numeric","font-variant-position","font-weight","grid","grid-area","grid-auto-columns","grid-auto-flow","grid-auto-rows","grid-column","grid-column-end","grid-column-gap","grid-column-start","grid-gap","grid-row","grid-row-end","grid-row-gap","grid-row-start","grid-template","grid-template-areas","grid-template-columns","grid-template-rows","hanging-punctuation","height","hyphens","image-rendering","@import","isolation","justify-content","@keyframes","left","letter-spacing","line-break","line-height","list-style","list-style-image","list-style-position","list-style-type","margin","margin-bottom","margin-left","margin-right","margin-top","max-height","max-width","@media","min-height","min-width","mix-blend-mode","object-fit","object-position","opacity","order","orphans","outline","outline-color","outline-offset","outline-style","outline-width","overflow","Specifies what happens if content overflows an element's box","overflow-wrap","overflow-x","overflow-y","padding","padding-bottom","padding-left","padding-right","padding-top","page-break-after","page-break-before","page-break-inside","perspective","perspective-origin","pointer-events","position","quotes","resize","right","tab-size","table-layout","text-align","text-align-last","text-combine-upright","text-decoration","text-decoration-color","text-decoration-line","text-decoration-style","text-indent","text-justify","text-orientation","text-overflow","text-shadow","text-transform","text-underline-position","top","transform","transform-origin","transform-style","transition","transition-delay","transition-duration","transition-property","transition-timing-function","unicode-bidi","user-select","vertical-align","visibility","white-space","widows","width","word-break","word-spacing","word-wrap","writing-mode","z-index"]
 
@@ -121,8 +131,35 @@ class Node {
 }
 
 class Sequence extends Node{
-  constructor() {
+  constructor(root = false) {
     super();
+    this.root = root;
+    this.nom = "";
+    this.mediaChilds = new Array();
+  }
+
+  isRoot(){
+    return this.root;
+  }
+
+  addMediaChild(c){
+    if(c) this.mediaChilds.push(c);
+  }
+
+  getMediaChilds(){
+    return this.mediaChilds;
+  }
+
+  haveMediaChilds(){
+    return this.mediaChilds.length > 0;
+  }
+
+  setNom(n){
+    this.nom = n.trim();
+  }
+
+  getNom(){
+    return this.nom;
   }
 
   toCSS(){
@@ -131,8 +168,6 @@ class Sequence extends Node{
     for (let c of this.childs) {
       c.toCSS(arr, new RegleCSS());
     }
-
-    console.log(arr);
 
     let str = "";
 
@@ -145,6 +180,10 @@ class Sequence extends Node{
 
         str += "}";
       }
+    }
+
+    for (let s of this.getMediaChilds()) {
+      if(s.haveChilds() || s.haveMediaChilds()) str += s.getNom() + '{' + s.toCSS() + '}';
     }
 
     return str;
@@ -212,8 +251,8 @@ class RegleCSS {
 
     for (let i = 0; i < arrParent.length; ++i) {
       for (let j = 0; j < arrEnfant.length; ++j) {
-        let spe = arrEnfant[j][0]
-        arr.push(arrParent[i] +((spe == ':' || spe == '[') ? "" : " ")+ arrEnfant[j]);
+        let spe = arrEnfant[j][0];
+        arr.push(arrParent[i] + ((spe == ':' || spe == '[' || arrParent[i] == '') ? "" : " ") + arrEnfant[j]);
       }
     }
 
@@ -254,7 +293,16 @@ class Attribut extends Node{
 }
 
 class KCSSReader {
-  constructor(text) {
+  constructor() {
+    this.text = null;
+    this.length = 0;
+    this.pointer = 0;
+    this.currentLine = 0;
+    this.currentCol = 0;
+    this.vars = null;
+  }
+
+  readKCSS(text){
     this.text = this.prePoc(text);
 
     this.length = text.length;
@@ -263,14 +311,14 @@ class KCSSReader {
     this.currentLine = 1;
     this.currentCol = 0;
 
-    this.currentChar = "";
-
     this.vars = new Array();
 
-    this.feuille = this.readSequence();
+    this.feuille = this.readSequence(true);
+  }
 
-    console.log(this.feuille);
-    console.log(this.feuille.toCSS());
+  writeCSS(){
+    if(this.feuille) return this.feuille.toCSS();
+    else return null;
   }
 
   prePoc(text){
@@ -297,7 +345,7 @@ class KCSSReader {
         }
       }
 
-      if(!commentChar && !wait) finalText += c1;
+      if((!commentChar && !wait) || c1 == '\n') finalText += c1;
     }
 
     return finalText;
@@ -336,7 +384,7 @@ class KCSSReader {
 
   isSimpleChar(c){
     let v = c ? c.charCodeAt(0) : c;
-    return (v >= 97 && v <= 122) || (v >= 65 && v <= 90) || (v >= 48 && v <= 57);
+    return (v >= 97 && v <= 122) || (v >= 65 && v <= 90) || (v >= 48 && v <= 57 || v == 95);
   }
 
   readNom(move = false){
@@ -364,23 +412,65 @@ class KCSSReader {
 //////////////////////////////////////////////////////////////
 
 
-  readSequence(){
-    const seq = new Sequence();
+  readSequence(root){
+    const seq = new Sequence(root);
 
     this.skipSpaces();
 
-    while (this.notEOF()) {
+    if(!root){
+      if(this.readChar() != '@') return null;
+
+      this.nextChar();
+
+      let nomFinal = "@";
+
+      let c = this.readChar();
+
+      while (this.isSimpleChar(c) || Words.mediaCharExist(c) || c == '?') {
+        if(c == '?') {
+
+        this.nextChar();
+        nomFinal += this.readVarVal();
+        c = this.readChar();
+
+        } else {
+          nomFinal += c;
+          this.nextChar();
+          c = this.readChar();
+        }
+      }
+
+      if(this.readChar() != '{') throw this.exception(`Jeton imprévu -> '${this.readChar()}'`);
+      this.nextChar();
+
+      seq.setNom(nomFinal);
+    }
+
+
+    this.skipSpaces();
+
+
+    while (this.notEOF() && !(!root && this.readChar() == '}')) {
 
       if(!this.readVar()){
-        let node = this.readBlockMedia();
-        if(!node) node = this.readBlock();
+        let node = this.readSequence();
+        if(node) seq.addMediaChild(node);
+        else {
+          node = this.readBlock();
 
-        if(!node) throw this.exception("Il y a une erreur autour de:");
+          if (node) seq.addChild(node);
 
-        seq.addChild(node);
+          else throw this.exception("Il y a une erreur autour de:");
+        }
       }
 
       this.skipSpaces();
+
+    }
+
+    if(!root){
+      if(this.readChar() != '}') throw this.exception(`Jeton imprévu -> '${this.readChar()}'`);
+      this.nextChar();
     }
 
     return seq;
@@ -396,7 +486,7 @@ class KCSSReader {
 
     this.skipSpaces();
 
-    if(this.readChar() != ':') throw this.exception(`Séparateur ':' manqant`);
+    if(this.readChar() != ':') throw this.exception(`Séparateur ':' manquant`);
 
     this.nextChar();
     this.skipSpaces();
@@ -477,7 +567,7 @@ class KCSSReader {
           }
         }
 
-        if(this.readChar() != ']') throw this.exception(`Jeton imprevu -> '${this.readChar()}'`);
+        if(this.readChar() != ']') throw this.exception(`Jeton imprévu -> '${this.readChar()}'`);
 
         nomFinal += ']';
 
@@ -508,7 +598,7 @@ class KCSSReader {
         } else if (type == 2) {
           nomFinal += buffer;
 
-          if(this.readChar() != '(') throw this.exception(`Jeton imprevu -> '${this.readChar()}'`);
+          if(this.readChar() != '(') throw this.exception(`Jeton imprévu -> '${this.readChar()}'`);
 
           nomFinal += '(';
 
@@ -530,14 +620,14 @@ class KCSSReader {
             }
           }
 
-          if(this.readChar() != ')') throw this.exception(`Jeton imprevu -> '${this.readChar()}'`);
+          if(this.readChar() != ')') throw this.exception(`Jeton imprévu -> '${this.readChar()}'`);
 
           nomFinal += ')';
 
           this.nextChar();
 
         } else {
-          throw this.exception(`Le selecteur '${buffer}' n'existe pas`);
+          throw this.exception(`Le sélecteur '${buffer}' n'existe pas`);
         }
       }
 
@@ -578,7 +668,7 @@ class KCSSReader {
         if (!node) {
           this.skipSpaces();
           if (this.readChar() != '}') {
-            throw this.exception(`Fermeture du block '${block.nom}' introuvable`);
+            throw this.exception(`Fermeture du bloc '${block.nom}' introuvable`);
           }
         }
       }
@@ -614,7 +704,7 @@ class KCSSReader {
 
     this.nextChar(offset);
     this.skipSpaces();
-    if (this.readChar() != ':') throw this.exception(`Séparateur ':' manqant`);
+    if (this.readChar() != ':') throw this.exception(`Séparateur ':' manquant`);
     this.nextChar();
     this.skipSpaces();
 
@@ -625,7 +715,7 @@ class KCSSReader {
       while (this.readChar() === ' ') this.nextChar();
       let c = this.readChar();
       if(c == ';' || c == '\n' || c == '}') this.nextChar();
-      else throw this.exception(`Jeton imprevu -> '${this.readChar()}'`);
+      else throw this.exception(`Jeton imprévu -> '${this.readChar()}'`);
 
     } else {
       let buffer = "";
@@ -644,10 +734,4 @@ class KCSSReader {
     return att;
   }
 
-
-  readBlockMedia(){
-    if(this.readChar() != '@') return null;
-
-    throw "non pris en charge";
-  }
 }
