e95fd6de4c0fa51aaf057052a61ed00c8b5edc66
CookieKastanie <jerem0205@yahoo.fr>
2018-09-12 23:07:50 +0200

    ...

---
+++ WIPGL/js/Texture.js
@@ -0,0 +1,39 @@
+class Texture {
+  constructor(display, img) {
+    this.ctx = display.ctx;
+
+    this.texture = this.ctx.createTexture();
+    this.ctx.bindTexture(this.ctx.TEXTURE_2D, this.texture);
+
+    this.ctx.bindTexture(this.ctx.TEXTURE_2D, this.texture);
+    this.ctx.texImage2D(this.ctx.TEXTURE_2D,
+      0, // niveau du bitmap
+      this.ctx.RGBA, //internalFormat
+      this.ctx.RGBA, //srcFormat
+      this.ctx.UNSIGNED_BYTE, //srcType
+      img
+    );
+
+    if (this.isPowerOf2(img.width) && this.isPowerOf2(img.height)) {
+       this.ctx.generateMipmap(this.ctx.TEXTURE_2D);
+    } else {
+       this.ctx.texParameteri(this.ctx.TEXTURE_2D, this.ctx.TEXTURE_WRAP_S, this.ctx.CLAMP_TO_EDGE /*this.ctx.REPEAT*/);
+       this.ctx.texParameteri(this.ctx.TEXTURE_2D, this.ctx.TEXTURE_WRAP_T, this.ctx.CLAMP_TO_EDGE /*this.ctx.REPEAT*/);
+       this.ctx.texParameteri(this.ctx.TEXTURE_2D, this.ctx.TEXTURE_MIN_FILTER, this.ctx.LINEAR);
+    }
+  }
+
+  isPowerOf2(val){
+    return (val & (val - 1)) == 0;
+  }
+
+  bind(unit = 0){
+    if (unit < 0 || unit > (this.ctx.MAX_COMBINED_TEXTURE_IMAGE_UNITS - 1)) {
+      console.error("Numero de texture invalide");
+      return;
+    }
+
+    this.ctx.activeTexture(this.ctx.TEXTURE0 + unit);
+    this.ctx.bindTexture(this.ctx.TEXTURE_2D, this.texture);
+  }
+}
--- WIPGL/js/main.js
+++ WIPGL/js/main.js
@@ -1,5 +1,55 @@
-const display = new Display(300, 300);
-
+const display = new Display(1000, 1000);
 display.setClearColor(0.3, 0.6, 0.0, 1.0);
 
-display.clear();
+
+
+const shader = new Shader(display, `
+precision mediump float;
+
+attribute vec3 position;
+attribute vec2 texCoord;
+
+varying vec2 texCoord0;
+
+void main(){
+  gl_Position = vec4(position, 1.0);
+  texCoord0 = texCoord;
+}
+`, `
+precision mediump float;
+
+varying vec2 texCoord0;
+uniform sampler2D diffuse;
+
+void main(){
+  gl_FragColor = texture2D(diffuse, texCoord0);
+}
+
+`);
+
+
+
+const mesh = new Mesh(display, [
+  -0.5, -0.5, 0.0, 0.0, 1.0,
+  0.0, 0.5, 0.0, 0.5, 0.0,
+  0.5, -0.5, 0.0, 1.0, 1.0
+], 3);
+
+
+const img = new Image();
+
+img.onload = () => {
+
+  const texture = new Texture(display, img);
+
+  shader.bind();
+
+  ///
+
+  display.clear();
+
+  texture.bind(0);
+  mesh.draw();
+}
+
+img.src = "imgs/leeroy.png";
