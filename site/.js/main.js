bfc2db0050522c6453800e352f8012c817ab21fb
CookieKastanie <jerem0205@yahoo.fr>
2018-11-21 13:38:36 +0100

    ...

--- site/.js/main.js
+++ site/.js/main.js
@@ -1,4 +1,4 @@
-const body = document.getElementsByTagName('body')[0];
+/*const body = document.getElementsByTagName('body')[0];
 
 const style = document.getElementsByTagName('style')[0];
 
@@ -18,4 +18,25 @@ for (let y = 0; y < innerWidth/100; ++y) {
     const t = document.createElement('div');
     body.append(t);
   }
+}*/
+
+
+/*
+const content = document.getElementById('content');
+
+const pages = document.getElementsByClassName('page');
+
+const offsetHeight = document.getElementsByTagName('ul')[0].offsetHeight;
+
+const menu = new Array().slice.call(document.getElementsByTagName('li'));
+
+for (let i in menu) {
+  const btn = menu[i];
+
+  btn.onclick = () => {
+    const p = pages[i];
+    console.log(p.offsetTop - offsetHeight);
+    window.scrollTo(0, p.offsetTop - offsetHeight);
+  }
 }
+*/
--- site/index.php
+++ site/index.php
@@ -1,24 +1,29 @@
-<?php include_once ".php/readDir.php"; ?>
-
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>CookieKastanie</title>
-    <style media="screen"></style>
+    <link rel="stylesheet" href=".css/main.css">
   </head>
   <body>
-    <!--<h3>Mes projets:</h3>
-    <?php
+    <ul id="menu">
+      <li><a href="#p1">1</a></li>
+      <li><a href="#p2">2</a></li>
+      <li><a href="#p3">3</a></li>
+    </ul>
 
-    /*$liste = readCurrentDir();
+    <div class="page" id="p1">
+      <img src=".medias/profil.png">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-    foreach ($liste as $e) {
-      echo "<a href=\"$e\">$e</a><br>";
-    }*/
+    </div>
+    <div class="page" id="p2">
 
-    ?>-->
+    </div>
+    <div class="page" id="p3">
+
+    </div>
+
   </body>
-
   <script type="text/javascript" src=".js/main.js"></script>
 </html>
