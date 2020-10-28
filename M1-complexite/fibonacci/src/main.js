c78c3d415aa45b9f4ce9154b947272941800bd8e
CookieKastanie <jerem0205@yahoo.fr>
2020-10-28 18:47:49 +0100

    ...

--- M1-complexite/fibonacci/src/main.js
+++ M1-complexite/fibonacci/src/main.js
@@ -1,41 +1,80 @@
+const fs = require('fs');
 const Fibonacci = require("./Fibonacci");
-/*
-let str = '';
-for(let i = 0; i <= 20; ++i) str += `${Fibonacci.recursive(i)}; `;
-console.log('recursive :', str);
-
-str = '';
-for(let i = 0; i <= 20; ++i) str += `${Fibonacci.iterative(i)}; `;
-console.log('iterative :', str);
-
-str = '';
-for(let i = 0; i <= 20; ++i) str += `${Fibonacci.exponentiation(i)}; `;
-console.log('exponent* :', str);
-*/
 
 const mesure = (f, n) => {
     const t0 = process.hrtime();
-    for(let i = 0; i < n; ++i) f(i);
+    f(n);
     const delta = process.hrtime(t0);
     return (delta[0] * 1e9 + delta[1]) * 1e-9;
 }
 
-const iter = 47;
+const benchmark = (f, bench) => {
+    const result = new Array();
 
-console.log("Start");
+    for(const n of bench) {
+        console.log(`\t\t-> ${n}`);
+        const time = mesure(f, n);
+        result.push(time);
+    }
 
-//const recTime = mesure(Fibonacci.recursive, iter);
-//const iterTime = mesure(Fibonacci.iterative, iter);
-//const expTime = mesure(Fibonacci.exponentiation, iter);
+    return result;
+}
 
-//console.log(`recursive : ${recTime} secondes`);
-//console.log(`iterative : ${iterTime} secondes`);
-//console.log(`exponent* : ${expTime} secondes`);
+const writeCSV = (bench, result, name) => {
+    let file = 'value\t';
+
+    const values = new Array();
+    values.push(bench);
+
+    for(const k of result.keys()) {
+        file += `${k}\t`;
+        values.push(result.get(k));
+    }
+
+    file += '\n';
+
+    for(let i = 0; i < bench.length; ++i) {
+        for(const v of values) {
+            file += `${v[i].toFixed(9).replace('.', ',')}\t`;
+        }
+        file += '\n';
+    }
+
+    fs.writeFileSync(`./${name}.tsv`, file, 'utf8');
+}
+
+const bench1 = [    1,     2,     3,     4,     5,     10,     15,     20,     25,     30,      35,      40,      45];
+const bench2 = [10000, 20000, 30000, 40000, 50000, 100000, 200000, 300000, 400000, 500000, 1000000, 1500000, 2000000];
+const bench3 = [ 1024,  2048,  4096,  8192, 16384,  32768,  65536, 131072, 262144, 524288, 1048576, 2097152, 4194304];
 
 
+const bench1Result = new Map();
+console.log('bench1 :');
+console.log('\trecursive');
+bench1Result.set('recursive', benchmark(Fibonacci.recursive, bench1));
+console.log('\titerative');
+bench1Result.set('iterative', benchmark(Fibonacci.iterative, bench1));
+console.log('\texponentiation');
+bench1Result.set('exponentiation', benchmark(Fibonacci.exponentiation, bench1));
+writeCSV(bench1, bench1Result, 'bench1');
+console.log('\n');
 
-const fs = require('fs');
-const t0 = process.hrtime();
-fs.writeFileSync('./fibo.txt', Fibonacci.exponentiation(500001)), 'utf8';
-const delta = process.hrtime(t0);
-console.log((delta[0] * 1e9 + delta[1]) * 1e-9);
+
+const bench2Result = new Map();
+console.log('bench2 :');
+console.log('\titerative');
+bench2Result.set('iterative', benchmark(Fibonacci.iterative, bench2));
+console.log('\texponentiation');
+bench2Result.set('exponentiation', benchmark(Fibonacci.exponentiation, bench2));
+writeCSV(bench2, bench2Result, 'bench2');
+console.log('\n');
+
+
+const bench3Result = new Map();
+console.log('bench3 :');
+console.log('\titerative');
+bench3Result.set('iterative', benchmark(Fibonacci.iterative, bench3));
+console.log('\texponentiation');
+bench3Result.set('exponentiation', benchmark(Fibonacci.exponentiation, bench3));
+writeCSV(bench3, bench3Result, 'bench3');
+console.log('\n');
