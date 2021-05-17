4149963b5275f0480e5589a98849a07505661972
CookieKastanie <jerem0205@yahoo.fr>
2021-05-17 19:24:32 +0200

    ...

--- texel/webpack.config.js
+++ texel/webpack.config.js
@@ -1,3 +1,31 @@
+class Without { // prend une liste de regex, et supprime les fichiers
+    constructor(patterns) {
+        this.patterns = patterns;
+    }
+
+    apply(compiler) {
+        compiler.hooks.emit.tapAsync("MiniCssExtractPluginCleanup", (compilation, callback) => {
+            Object.keys(compilation.assets).filter(asset => {
+                    let match = false;
+
+                    let i = this.patterns.length;
+                    while (i--) {
+                        if (this.patterns[i].test(asset)) {
+                            match = true;
+                        }
+                    }
+
+                    return match;
+                }).forEach(asset => {
+                    delete compilation.assets[asset];
+                });
+
+            callback();
+        });
+    }
+}
+
+
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -7,7 +35,6 @@ const config = {
     mode: 'development',
     entry: {
         bundle: path.resolve(__dirname, 'src/main.js'),
-        //loading: path.resolve(__dirname, 'src/css/importLoadingCSS.js')
         loading: path.resolve(__dirname, 'src/css/loading.css')
     },
     output: {
@@ -26,7 +53,8 @@ const config = {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
     plugins: [
-        new MiniCssExtractPlugin({filename: '[name].css'})
+        new MiniCssExtractPlugin({filename: '[name].css'}),
+        new Without([/loading.js/]) // supprime loading.js car inutile
     ]
 };
 
