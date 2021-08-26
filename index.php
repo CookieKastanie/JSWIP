d1aa8988ef038499bde772a578dc8e2699db48bd
CookieKastanie <jerem0205@yahoo.fr>
2021-08-26 16:48:40 +0200

    ...

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <?php
 
 function lireDossier($chemin, $exept = array()){
-    $liste = array();
+    $list = array();
     $index = 0;
 
     if ($dossier = opendir($chemin)){
         while ($f = readdir($dossier)){
             if ($f != "." && $f != ".." && !in_array($f, $exept) && $f[0] != '.'){
-                $liste[$index++] = $f;
+                $list[$index++] = $f;
             }
         }
 
         closedir($dossier);
     }
 
-    return $liste;
+    return $list;
   }
+
+
+    function displayList($list) {
+        foreach($list as $e) {
+            echo '<a class="list-group-item" href="'.$e.'" target="_blank">'.ucfirst($e).'</a>';
+        }
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -27,23 +35,61 @@ function lireDossier($chemin, $exept = array()){
         <title>CookieKastanie</title>
         <link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.min.css">
         <style media="screen">
-          a{color: white;}
-          a:hover{color: white;}
+          a {color: white;}
+          a:hover {color: white;}
+
+          .list-group {
+            margin-bottom: 20px;
+          }
+
+          .g1 .list-group-item {
+            background: rgba(168, 102, 255, 0.2);
+            border-color: rgba(168, 102, 255, 0.5);
+          }
+
+          .g2 .list-group-item {
+            background: rgba(133, 251, 119, 0.2);
+            border-color: rgba(133, 251, 119, 0.5);
+          }
+
+          .g3 .list-group-item {
+            background: rgba(101, 248, 255, 0.2);
+            border-color: rgba(101, 248, 255, 0.5);
+          }
         </style>
     </head>
     <body>
         <div class="container">
             <h2>CookieKastanie</h2>
+
+            <div class="list-group g1">
+                <?php
+                    $group1 = array('texel', 'couleurs', 'flex', 'bot_manager', 'MachineDeTuring');
+                    displayList($group1);
+                ?>
+            </div>
+
+            <div class="list-group g2">
+                <?php
+                    $group2 = array('gameOfLife', 'balles', 'snake', 'p_test_1', '2dRaycast');
+                    echo '<a class="list-group-item" href="https://ljdmdv.letoutchaud.fr" target="_blank" >LJDMDV</a>';
+                    displayList($group2);
+                ?>
+            </div>
+
+            <div class="list-group g3">
+                <?php
+                    $group3 = array('splines', 'WIPCE', 'trails', 'mandelbrot', 'bubulles', 'noise');
+                    displayList($group3);
+                ?>
+            </div>
+
             <div class="list-group">
                 <?php
+                    $list = lireDossier("./", array("index.php", "favicon.ico", "CV", "Tests", "WIPGL", "Site16mb", "404", ...$group1, ...$group2, ...$group3));
+                    sort($list);
 
-                $liste = lireDossier("./", array("index.php", "favicon.ico", "CV", "Tests", "WIPGL", "Site16mb", "404"));
-                sort($liste);
-
-                foreach ($liste as $e) {
-                    echo "<a class=\"list-group-item\" href=\"$e\">$e</a>";
-                }
-
+                    displayList($list);
                 ?>
 
             </div>
